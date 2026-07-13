961684790a1e18833dda2e896ad103db67f39d0f
royalsenpai0-art <royalsenpai0@gmail.com>
2026-07-13 21:12:13 +0500

    Fix homepage cache and update latest games

--- animeverse/app/page.tsx
+++ animeverse/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Link from "next/link";
 import Image from "next/image";
+export const dynamic = "force-dynamic";
 import InPagePush from "@/components/InPagePush";
 import Hero from "@/components/Hero";
 import WhatsAppSticky from "@/components/WhatsAppSticky";
